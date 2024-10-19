47fc3fe103098606ff0ec58216b9305221008a89
Rodney Taylor <rod.taylor@ba.com>
2024-10-19 05:40:14 +0100

    1st application: Init commit

---
+++ app1/backend/banking/src/test/java/com/rtaylor02/apifirst/banking/SimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtaylor02.apifirst.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
